7815f6ec971c087499148132cd24a343f3a1ccd9
Avnish Choubey <118668098+AvnishChoubey@users.noreply.github.com>
2025-10-21 13:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+document.getELementById('submit').addEventListener('click', () => {
+	const username = document.getElementById('username').value;
+	cont password = document.getElementById('password').value;
+	const rememberme = document.getELementById('checkbox');
+	if(rememberme.checked) {
+		localstorage.setItem("username", username);
+		localstorage.setItem("password", password);
+	} else {
+		localstorage.clear();
+	}
+	alert(`Logged in as ${username}`);
+});
+
+document.getElementById('existing').addEventListener('click', () => {
+	const username = localStorage.getItem("username");
+	alert(`Logged in as ${username}`);
+})
+
+function initialFunction() {
+	if(localStorage.getItem("username") != null)
+	document.getELementById('existing').style.display = block;
+}
+
+initialFunction();
+
